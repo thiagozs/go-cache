48d9c49da4b5faf0a55c7de77675f46dfb65cb3e
Thiago Zilli <thiago.zilli@gmail.com>
2021-11-18 12:12:36 -0300

    First commit

---
+++ cache.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/thiagozs/go-utils/files"
+	"github.com/tidwall/buntdb"
+)
+
+type CacheRepo interface {
+	WriteKeyVal(key string, val string) error
+	WriteKeyValTTL(key string, val string, ttlSeconds int) error
+	DeleteKey(key string) (string, error)
+	WriteKeyValAsJSON(key string, val interface{}) error
+	WriteKeyValAsJSONTTL(key string, val interface{}, ttlSeconds int) error
+	GetVal(key string) (string, error)
+}
+type buntdblayer struct {
+	db     *buntdb.DB
+	file   string
+	folder string
+	ttl    int
+	log    zerolog.Logger
+}
+
+func New(folder, file, ttl string,
+	debug bool) (CacheRepo, error) {
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log := zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+	if debug {
+		log.Info().Bool("debug", true).Msg("[CACHE]")
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	pathFile := fmt.Sprintf("%s/%s", folder, file)
+
+	log.Info().Str("path_file", pathFile).Msg("[CACHE]")
+	if !files.FileExists(pathFile) {
+		if err := files.MkdirAll(path.Dir(pathFile)); err != nil {
+			log.Info().Err(err).Msg("Fail create a directory")
+			return nil, err
+		}
+	}
+
+	db, err := buntdb.Open(pathFile)
+	if err != nil {
+		log.Info().Err(err).Msg("Could not open data file path")
+		return nil, err
+	}
+	ttlInt, _ := strconv.Atoi(ttl)
+	return &buntdblayer{
+		db:     db,
+		folder: folder,
+		file:   file,
+		ttl:    ttlInt,
+		log:    log,
+	}, nil
+}
+
+func (d *buntdblayer) GetVal(key string) (string, error) {
+	var value string
+	err := d.db.View(func(tx *buntdb.Tx) error {
+		val, err := tx.Get(key)
+		if err != nil {
+			d.log.Debug().Err(err).Msg("GetVal")
+			return err
+		}
+		value = val
+		return nil
+	})
+	return value, err
+}
+
+func (d *buntdblayer) DeleteKey(key string) (string, error) {
+	var value string
+	err := d.db.Update(func(tx *buntdb.Tx) error {
+		val, err := tx.Delete(key)
+		if err != nil {
+			d.log.Debug().Err(err).Msg("DeleteKey")
+			return err
+		}
+		value = val
+		return nil
+	})
+	return value, err
+}
+
+func (d *buntdblayer) WriteKeyVal(key string, val string) error {
+	err := d.db.Update(func(tx *buntdb.Tx) error {
+		_, _, err := tx.Set(key, val, nil)
+		d.log.Debug().Err(err).Msg("WriteKeyVal")
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *buntdblayer) WriteKeyValTTL(key string, val string, ttlSeconds int) error {
+	if ttlSeconds == 0 {
+		d.log.Debug().Int("ttl_seconds", d.ttl).Msg("WriteKeyValTTL")
+		ttlSeconds = d.ttl
+	}
+	err := d.db.Update(func(tx *buntdb.Tx) error {
+		_, _, err := tx.Set(key, val, &buntdb.SetOptions{Expires: true, TTL: time.Second * time.Duration(ttlSeconds)})
+		d.log.Debug().Str("method", "update").Err(err).Msg("WriteKeyValTTL")
+		return err
+	})
+	if err != nil {
+		d.log.Debug().Err(err).Msg("WriteKeyValTTL")
+		return err
+	}
+	return nil
+}
+
+func (d *buntdblayer) WriteKeyValAsJSON(key string, val interface{}) error {
+	valueAsJSON, err := json.Marshal(val)
+	if err != nil {
+		d.log.Debug().Str("method", "json.Marshal").Err(err).Msg("WriteKeyValAsJSON")
+		return err
+	}
+	return d.WriteKeyVal(key, string(valueAsJSON))
+}
+
+func (d *buntdblayer) WriteKeyValAsJSONTTL(key string, val interface{}, ttlSeconds int) error {
+	valueAsJSON, err := json.Marshal(val)
+	if err != nil {
+		d.log.Debug().Str("method", "json.Marshal").Err(err).Msg("WriteKeyValAsJSONTTL")
+		return err
+	}
+	return d.WriteKeyValTTL(key, string(valueAsJSON), ttlSeconds)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/thiagozs/go-cache
+
+go 1.17
+
+require (
+	github.com/rs/zerolog v1.26.0
+	github.com/thiagozs/go-utils v0.0.0-20211118150243-5cfe9a632a4b
+	github.com/tidwall/buntdb v1.2.7
+)
+
+require (
+	github.com/tidwall/btree v0.6.1 // indirect
+	github.com/tidwall/gjson v1.10.2 // indirect
+	github.com/tidwall/grect v0.1.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/rtred v0.1.2 // indirect
+	github.com/tidwall/tinyqueue v0.1.1 // indirect
+)
